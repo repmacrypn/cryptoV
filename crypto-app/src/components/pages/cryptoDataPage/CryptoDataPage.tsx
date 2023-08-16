4136ec2b8c1cc50692fe4f226f0b15668216a1db
repmacrypn <artik3267@gmail.com>
2023-08-16 15:31:10 +0300

    diagram injected

--- crypto-app/src/components/pages/cryptoDataPage/CryptoDataPage.tsx
+++ crypto-app/src/components/pages/cryptoDataPage/CryptoDataPage.tsx
@@ -1,8 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
+import { ChartDataset } from 'chart.js'
+import { Line } from 'react-chartjs-2'
 import { NavLink, useParams } from 'react-router-dom'
 import { cryptoAPI } from 'src/services/clientService'
+import { AssetHistory } from 'src/types/assetHistory.interface'
 import { CryptoCoin } from 'src/types/cryptocoin.interface'
 import { HISTORY_INTERVAL } from 'src/utils/constantData'
+import { Chart, registerables } from 'chart.js'
 
 export const CryptoDataPage = () => {
     const { id } = useParams()
@@ -17,11 +21,41 @@ export const CryptoDataPage = () => {
         queryKey: ['assetsHistory', id],
         queryFn: () => cryptoAPI.fetchAssetHistory(id!, HISTORY_INTERVAL),
         staleTime: Infinity,
+        enabled: !!asset.id,
     })
 
-    if (isLoading) return <div>qq</div>
+    const labels = history.map((curTimeObj: AssetHistory) => new Date(curTimeObj.time).toLocaleString())
+    const datasets = history.map((curTimeObj: AssetHistory) => curTimeObj.priceUsd)
 
-    console.log(history)
+    Chart.register(...registerables)
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'crypto data history',
+                fill: false,
+                lineTension: 0.1,
+                backgroundColor: 'rgba(75,192,192,0.4)',
+                borderColor: 'rgba(75,192,192,1)',
+                borderCapStyle: 'butt',
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: 'miter',
+                pointBorderColor: 'rgba(75,192,192,1)',
+                pointBackgroundColor: '#fff',
+                pointBorderWidth: 1,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+                pointHoverBorderColor: 'rgba(220,220,220,1)',
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: datasets,
+            } as ChartDataset<'line', string[]>,
+        ],
+    }
+
+    if (isLoading) return <div>qq</div>
 
     return (
         <div>
@@ -35,6 +69,9 @@ export const CryptoDataPage = () => {
                 <div>vwap24Hr: {asset.vwap24Hr}</div>
                 <div>volumeUsd24Hr: {asset.volumeUsd24Hr}</div>
                 <div>+</div>
+            </div>
+            <div>
+                <Line data={data} />
             </div>
             <NavLink to='/overviewPage'>
                 <div>
