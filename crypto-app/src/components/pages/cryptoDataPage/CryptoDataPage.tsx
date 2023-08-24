cd494e3be9e9cb3619b34b34305084ac9036e378
repmacrypn <artik3267@gmail.com>
2023-08-25 01:56:16 +0300

    crypto data page refactoring

--- crypto-app/src/components/pages/cryptoDataPage/CryptoDataPage.tsx
+++ crypto-app/src/components/pages/cryptoDataPage/CryptoDataPage.tsx
@@ -1,11 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import { ChartDataset } from 'chart.js'
-import { Chart, registerables } from 'chart.js'
-import { Line } from 'react-chartjs-2'
 import { useParams } from 'react-router-dom'
+import { AssetShortInfo } from './short asset info/AssetShortInfo'
 import s from './CryptoDataPage.module.scss'
+import { Diagram } from './diagram/Diagram'
 import { cryptoAPI } from 'src/services/clientService'
-import { AssetHistory } from 'src/types/assetHistory.interface'
 import { CryptoCoin } from 'src/types/cryptocoin.interface'
 import { HISTORY_INTERVAL } from 'src/utils/constantData'
 import { ControlWrapper } from 'src/components/control/Control'
@@ -28,24 +26,14 @@ export const CryptoDataPage = () => {
         enabled: !!asset.id,
     })
 
-    const assetInfo = Object.entries(asset).map(a => {
-        if (a[0] === 'explorer') return
-
-        return (
-            <AssetInfo
-                key={a[0]}
-                text={a[0]}
-                value={a[1]}
-            />
-        )
-    })
-
     if (isLoading) return <Preloader />
 
     return (
         <>
             <div className={s.assetInfo}>
-                {assetInfo}
+                <AssetShortInfo
+                    asset={asset}
+                />
                 <ControlWrapper
                     asset={asset}
                 />
@@ -56,59 +44,5 @@ export const CryptoDataPage = () => {
             />
             <BackToMain />
         </>
-    )
-}
-
-export const AssetInfo = ({ text, value }: { text: string; value: string; }) => {
-    return (
-        <div className={s.info}>
-            <div>{text}</div>
-            <div>{value || 'none'}</div>
-        </div>
-    )
-}
-
-interface DiagramProps {
-    name: string;
-    history: AssetHistory[];
-}
-
-export const Diagram = ({ history, name }: DiagramProps) => {
-
-    const labels = history.map((curTimeObj: AssetHistory) => new Date(curTimeObj.time).toLocaleString())
-    const datasets = history.map((curTimeObj: AssetHistory) => curTimeObj.priceUsd)
-
-    Chart.register(...registerables)
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: `${name} chart history for the last month`,
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: 'rgba(75,192,192,0.4)',
-                borderColor: 'rgba(75,192,192,1)',
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                pointBorderColor: 'rgba(75,192,192,1)',
-                pointBackgroundColor: '#fff',
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-                pointHoverBorderColor: 'rgba(220,220,220,1)',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: datasets,
-            } as ChartDataset<'line', string[]>,
-        ],
-    }
-
-    return (
-        <div>
-            <Line data={data} />
-        </div>
     )
 }
---
+++ crypto-app/src/components/pages/cryptoDataPage/short asset info/AssetShortInfo.tsx
@@ -0,0 +1,29 @@
+import s from './AssetShortInfo.module.scss'
+import { CryptoCoin } from 'src/types/cryptocoin.interface'
+
+export const AssetShortInfo = ({ asset }: { asset: CryptoCoin }) => {
+    const assetInfo = Object.entries(asset).map((a: [string, string]) => {
+        if (a[0] === 'explorer') return
+
+        return (
+            <AssetInfo
+                key={a[0]}
+                text={a[0]}
+                value={a[1]}
+            />
+        )
+    })
+
+    return (
+        <>{assetInfo}</>
+    )
+}
+
+export const AssetInfo = ({ text, value }: { text: string; value: string; }) => {
+    return (
+        <div className={s.info}>
+            <div>{text}</div>
+            <div>{value || 'none'}</div>
+        </div>
+    )
+}
